2bebbca46addf3b75446ddb1669c83d158131baf
nana07j <164012576+nana07j@users.noreply.github.com>
2024-09-24 10:51:26 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+criarCartao(categoria, pergunta, resposta) {
+
+}
+function CriarCartao(categoria, pergunta, resposta) {
+   let container = document.getElementById('container')
+   let cartao = document.createElement('article')
+   cartao.className = 'cartao'
+}
+cartao.innerHTML =
+`<div class="cartao_conteudo"></div>
+<h3>Programação</h3>
+<div class="cartao_conteudo_pergunta">
+    <p></p>O que é JavaScript?</p>
+</div>
+<div class="cartao_conteudo_resposta">
+    <p>O JavaScript é uma linguagem da programação</p>
+    </div>
+    </div>`
+    CSSContainerRule.appenChild(cartao)
